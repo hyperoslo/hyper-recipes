e0b725a4a04e35fd54c6f9e80f9fa6e51c0e4906
Johannes Gorset <jgorset@gmail.com>
2014-01-28 12:56:18 +0100

    Add validation to length of Recipe#name, descriptioin, instructions

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ActiveRecord::Base
   validates :name, presence: true
   validates :difficulty, presence: true, inclusion: { in: 1..3 }
+  validates :name, :description, :instructions, length: { maximum: 255 }
   mount_uploader :photo, PhotoUploader
 end
