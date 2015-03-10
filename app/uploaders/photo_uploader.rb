c24451630f5029096f17f298d8dd093ab3fcf62e
Ole Henrik Skogstrøm <ole.henrik@hyper.no>
2015-03-10 15:27:35 +0100

    Add thumbnail to recipes

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -4,7 +4,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+   include CarrierWave::MiniMagick
 
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
   # include Sprockets::Helpers::RailsHelper
@@ -22,6 +22,10 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  version :thumbnail do
+    process resize_to_fill: [200,200]
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
